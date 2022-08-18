be331097a2500193a341321432dfe629d3952d97
saulocapistrano <saulocapistrano@gmail.com>
2022-08-17 21:52:31 -0300

    repositories implementados

---
+++ src/main/java/com/bookstore/repository/LivroRepository.java
@@ -0,0 +1,11 @@
+package com.bookstore.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bookstore.domain.Livro;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Integer> {
+
+}
